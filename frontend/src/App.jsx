6eadefe549ac081e6f2e3e61035b578716319ff9
anirudh-singh01 <anirudhsingh.ani321@gmail.com>
2026-02-04 22:24:09 +0530

    video-conatiner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,9 +30,17 @@ function App() {
         <article>
           <h2>It's a me, Mario</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit commodi sunt fugit optio asperiores quae dignissimos quas deleniti quam. Laudantium vel debitis maiores doloribus commodi explicabo esse minima laborum ad.
+            Welcome to Mario Club — a fan-made space for everyone who grew up jumping on
+    Goombas, collecting coins, and racing through the Mushroom Kingdom. Share
+    your memories, post your thoughts, and connect with fellow Mario fans from
+    around the world.
           </p>
         </article>
+        <div className='trailer-container'>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/TnGl01FkMMo?si=CTpjodIdXgFxyAJT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/FdL2GorGdKc?si=d7YX-KSaoC7ofkOj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
         <ul className='images'>
           <li><img src={img.thumb1} alt="mario thumb 1" /></li>
           <li><img src={img.thumb2} alt="mario thumb 2" /></li>
@@ -43,7 +51,8 @@ function App() {
           Join today!
         </h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, libero? Aut labore veritatis, at rem beatae quae quaerat cupiditate cum soluta, maiores omnis dignissimos obcaecati perferendis voluptatem sunt minus velit?
+          A power up your fandom and become part of the Mario Club. Get updates, share
+    your thoughts, and connect with fans who love Mario as much as you do.
         </p>
         <form>
           <input type="email" name='email' placeholder='Type email & hit enter' required/>
@@ -51,7 +60,7 @@ function App() {
       </section>
       <footer>
         <p className='copyright'>
-          Copyright 2019 Marioclub
+          © {new Date().getFullYear()} Mario Club. Fan-made project. Not affiliated with Nintendo.
         </p>
       </footer>
     </div>
